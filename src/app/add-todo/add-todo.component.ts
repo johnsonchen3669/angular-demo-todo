5bd051a28e8aa7cc43272bd438e8c6f8f7e1eb46
tttom3669 <tttom3669@gmail.com>
2025-09-11 21:38:13 +0800

    day 11 event 練習

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ITodoItem } from '../todo-list/todo-item/todo-item.model';
 
 @Component({
   selector: 'app-add-todo',
   imports: [],
   templateUrl: './add-todo.component.html',
-  styleUrl: './add-todo.component.css'
+  styleUrl: './add-todo.component.css',
 })
 export class AddTodoComponent {
-
+  tempArr = signal<ITodoItem[]>([]);
+  addTodo() {
+    this.tempArr.update((todos) => [
+      ...todos,
+      { id: Date.now(), text: '待辦清單', completed: false },
+    ]);
+    console.log(this.tempArr());
+  }
 }
